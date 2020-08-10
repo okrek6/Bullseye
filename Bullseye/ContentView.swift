bfbfa4967f80b0e0af244077283acfc0f501ca57
Brendan Krekeler <bfkyb4@mail.missouri.edu>
2020-08-09 23:59:45 -0500

    First Commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Brendan Krekeler on 8/7/20.
+//  Copyright © 2020 Brendan Krekeler. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var alertIsVisiable: Bool = false
+    
+    var body: some View {
+        VStack {
+            VStack {
+                //Target row
+                HStack {
+                    Text("Put the bullseye as close as you can to: ")
+                    Text(/*@START_MENU_TOKEN@*/"100"/*@END_MENU_TOKEN@*/)
+                }
+                
+                //Slider row
+                HStack{
+                    Text("1")
+                    Slider(value: .constant(10))
+                    Text("100")
+                }
+                //button row
+                Button(action: {
+                    print("Button pressed")
+                    self.alertIsVisiable = true
+                }) {
+                  Text(/*@START_MENU_TOKEN@*/"Hit me!"/*@END_MENU_TOKEN@*/)
+                }
+                .alert(isPresented: $alertIsVisiable) { () ->
+                    Alert in
+                    return Alert(title: Text("Hello there!"), message: Text("This is my first popup."), dismissButton: .default(Text("Awesome!")))
+                }
+                
+                //score row
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().previewLayout(.fixed(width: 896, height: 414
+            ))
+    }
+}
